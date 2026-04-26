e1ba65cbf28b6c5211903261665e3d84d77406da
waniya farooq <waniyafarooq18@gmail.com>
2026-04-26 15:33:13 +0500

    Add task management views, templates, and URL configurations

--- djangop1/tasks/views.py
+++ djangop1/tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 from . import templates
+tasks = ["cook","study","tvd"]
+
 # Create your views here.
+
+def index(request):
+    return render(request,"tasks/index.html",{"tasks":tasks})
+
+def add(request):
+    return render(request,"tasks/add.html",{
+        "tasks":tasks
+    })
